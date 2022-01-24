55c798fe220b2fc37ac4d103253ac15ffef38ff6
Pedro Almeida <pealmeid@gmail.com>
2022-01-24 01:50:02 -0300

    CORS + auth

--- Repro/Controllers/WeatherForecastController.cs
+++ Repro/Controllers/WeatherForecastController.cs
@@ -1,9 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
 
 namespace Repro.Controllers
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route("api")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -18,8 +21,11 @@ namespace Repro.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+
+        // this call is validated, as expected
+        [RequiredScope("right_scope")]
+        [HttpGet("Method1")]
+        public IEnumerable<WeatherForecast> Get1()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -28,6 +34,35 @@ namespace Repro.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        // this call is not validated, as expected
+        [RequiredScope("wrong_scope")]
+        [HttpGet("Method2")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+
+        // this call is always validated, no matter what scope in set in appsettings.json
+        // it should only validate if that scope is `right_scope`
+        [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+        [HttpGet("Method3")]
+        public IEnumerable<WeatherForecast> Get3()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
         }
     }
 }
--- Repro/Program.cs
+++ Repro/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+const string myCorsPolicy = "_myCorsPolicy";
+const string clientHost = "https://localhost:44332";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    // CORS policy to allow call from client SPA:
+
+    options.AddPolicy(name: myCorsPolicy,
+        b =>
+        {
+            b.WithOrigins(clientHost)
+                .AllowAnyMethod()
+                .WithHeaders("authorization", "content-type");
+        });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,6 +42,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(myCorsPolicy);
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
